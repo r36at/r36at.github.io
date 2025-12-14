8cce4a75fdf717eb2158f86901f4aa57f717cb38
r36at <wyvern.claw@gmail.com>
2025-12-14 14:12:17 +0900

    deploy to github

---
+++ src/components/pages/Biography.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Biography: React.FC = () => {
+  return (
+    <div>
+      <h1>Biography</h1>
+      <p>This is the biography page.</p>
+    </div>
+  );
+};
+
+export default Biography;
---
+++ src/components/pages/Help.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Help: React.FC = () => {
+  return (
+    <div>
+      <h1>Help</h1>
+      <p>This is the help page.</p>
+    </div>
+  );
+};
+
+export default Help;
